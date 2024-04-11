eed932ffe85c8aaee8e7a877a0224f987fd95a80
uday112233 <y.udaykanth@gmail.com>
2024-04-11 19:52:43 +0530

    4

--- mysqlinstall.sh
+++ mysqlinstall.sh
@@ -5,6 +5,12 @@ if [ $userid -ne 0 ]
 then
 echo permission denied
 exit 1
+fi
+yum install mysql -y
+if [ $? -ne 0]
+then 
+echo check there is fault
+exit 1
 else
-yum install git -y
+echo installation is success
 fi
