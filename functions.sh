ad8b1b922745e73c1b1e013d85f23f949a73e6ab
uday112233 <y.udaykanth@gmail.com>
2024-04-12 07:09:27 +0530

    5

---
+++ functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+DATE=$(date %F)
+SCRIPT-NAME=$0
+LOGFILE=/tmp/$SCRIPT-NAME-$DATE.log
+
+R="\e[31m"
+G="\e[3m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+    echo -e "$2 ... $R FAILURE $N "
+     else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+# else
+#     echo "INFO:: You are root user"
+fi
+
+# it is our responsibility again to check installation is success or not
+yum install mysql -y &>>$LOGFILE
+
+VALIDATE $? "Installing MySQL"
+
+yum install posddtfix -y &>>$LOGFILE
+
+VALIDATE $? "Installing postfix"
